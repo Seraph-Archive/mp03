4f9c175caa1ac3667d9f9755e5f04c6d35fef0cf
Seraphina <etanschool@gmail.com>
2022-05-12 23:00:37 -0400

    reworked code so system works

--- script.js
+++ script.js
@@ -2,76 +2,48 @@
 var generateBtn = document.querySelector("#generate");
 
 //password elements
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var symbols = ["`", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", ",", ".", "<", ">", "'", ":", ";", "-", "_", "/", "?", "=", "+"];
-console.log(lowercase)
-console.log(uppercase)
-console.log(symbols)
+var numbers = "0123456789";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var symbols = "!@#$%^&*()-_=+{[}],.';:";
+
+//password elements shifted into arrays
+var numArr = numbers.split("");
+var lowArr = lowercase.split("");
+var upArr = uppercase.split("");
+var symArr = symbols.split("");
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var passLength = window.prompt("How many characters would you like your password?");
-  var passNum = window.confirm("Would you like numbers?");
-  var passLow = window.confirm("Would you like lowercase letters?");
-  var passUpper = window.confirm("Would you like uppercase letters?");
-  var passSym = window.confirm("Would you like special characters?");
   var maxPass = "";
+  var chonkArray = [];
 
-  console.log(passLength);
-  console.log(passNum);
-  console.log(passLow);
-  console.log(passUpper);
-  console.log(passSym);
+  var passLen = prompt("How long would you like your password to be?");
 
-  function generatePassword(passLength){
-  var maxPass;
-    if (passNum, passLow, passUpper, passSym){
-      var maxPass = maxPass.concat(numbers, lowercase, uppercase, symbols);
-      console.log(maxPass);
-    } else if (passNum, passLow, passUpper) {
-      var maxPass = maxPass.concat(numbers, lowercase, uppercase);
-      console.log(maxPass);
-    } else if (passNum, passLow, passSym){
-      var maxPass = maxPass.concat(numbers, lowercase, symbols);
-      console.log(maxPass)
-    } else if (passNum, passUpper, passSym) {
-      var maxPass = maxPass.concat(numbers, uppercase, symbols)
-    } else if (passLow, passUpper, passSym) {
-      var maxPass = maxPass.concat(lowercase, uppercase, symbols)
-    } else if (passLow, passSym) {
-      var maxPass = maxPass.concat(lowercase, symbols)
-    } else if (passUpper, passSym) {
-      var maxPass = maxPass.concat(uppercase, symbols)
-    } else if (passNum, passLow) {
-      var maxPass = maxPass.concat(numbers, lowercase)
-    } else if (passNum, passUpper) {
-      var maxPass = maxPass.concat(numbers, uppercase)
-    } else if (passNum, passSym) {
-      var maxPass = maxPass.concat(numbers, symbols)
-    } else if (passLow) {
-      var maxPass = maxPass.concat(lowercase)
-    } else if (passUpper) {
-      var maxPass = maxPass.concat(uppercase)
-    } else if (passNum) {
-      var maxPass = maxPass.concat(numbers)
-    } else if (passSym) {
-      var maxPass = maxPass.concat(symbols)
-    } else {
-      
-    }
-      for (var i = 0; i < passLength; i++) {
-        var pass = Math.floor(Math.random() * maxPass.length);
-        console.log(pass)
-      } return 
+  if(confirm("Would you like your password to contain upper case letters?")){
+    Array.prototype.push.apply(chonkArray, upArr);
   }
 
+if(confirm("Would you like your password to contain lower case letters?")){
+    Array.prototype.push.apply(chonkArray, lowArr);
+}
 
+if(confirm("Would you like your password to contain numbers?")){
+    Array.prototype.push.apply(chonkArray, numArr);
+}
 
-  passwordText.value = password;
+if(confirm("Would you like your password to contain symbols?")){
+    Array.prototype.push.apply(chonkArray, symArr);
+}
+
+for(var i=0; i<passLen; i++){
+  var random = Math.floor(Math.random()*chonkArray.length);
+  maxPass += chonkArray[random];
+}
+
+  document.getElementById("password").innerHTML = maxPass;
 }
 
 
